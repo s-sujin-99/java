14fb420203fdfb81efca73f91286d53b42359a4a
s-sujin-99 <t2684258@gmail.com>
2026-06-12 18:00:51 +0900

    [update] TimeWaiting 상태에서 interrupt 설정 테스팅2

--- chapter15/src/chapter15/page50/MyThread.java
+++ chapter15/src/chapter15/page50/MyThread.java
@@ -10,8 +10,8 @@ public class MyThread extends Thread{
 		} catch (InterruptedException e) {
 			//일시 정지 상태에서 interrupt 발생하면 메세지와 시간지연
 			System.out.printf("****sleep() 실행중 interrupt 발생****\n");
-			for (long i = 0; i < 20_000_000_000L; i++) {}
 		}
+		for (long i = 0; i < 20_000_000_000L; i++) {}
 	
 	} // end of run
 }
